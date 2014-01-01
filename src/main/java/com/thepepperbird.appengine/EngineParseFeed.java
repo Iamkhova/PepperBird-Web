b9c0f5cb15ada2c1f67de1adc677a4b231e4d4d0
Kareem Glover <iamkhova@gmail.com>
2014-01-01 23:10:40 +0000

    Update 101- split out handling of automated request and attached cron file.

--- src/main/java/com/thepepperbird.appengine/EngineParseFeed.java
+++ src/main/java/com/thepepperbird.appengine/EngineParseFeed.java
@@ -36,26 +36,34 @@ public class EngineParseFeed extends HttpServlet
  public void doGet(HttpServletRequest req, HttpServletResponse resp)throws IOException
   {
       resp.setContentType("text/plain");
-      
       BlogHandler bhh = new BlogHandler();
       SocialHandler social = new SocialHandler();
       CredentialHandler secure = new CredentialHandler();
       
-
-      //Parse Feeds - Convert RSS to JSON and stored in Datastore
-      social.rssParse2DB("http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=8&q=http://www.google.com/alerts/feeds/14472321683254243665/11564968275061407926");
-      social.rssParse2DB("http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=8&q=http://allafrica.com/tools/headlines/rdf/liberia/headlines.rdf");
-      social.rssParse2DB("http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=8&q=http://www.google.com/alerts/feeds/14472321683254243665/7898718268626814814");
+      String value = req.getParameter("value");
+      log.info("value" + value);
       
-      //Push Feeds to Blogger
-      try {
-             social.syncDB2Blogger(secure.getBlogID(secure.LIBERIA_NEWS_BLOG));
-          } catch (Exception e) {
-              log.info("failed" + e);
+      if ("syncRSS".equals(value))
+      {
+          //Parse Feeds - Convert RSS to JSON and stored in Datastore
+          social.rssParse2DB("http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&q=http://www.google.com/alerts/feeds/14472321683254243665/10900860890542401257");
+          social.rssParse2DB("http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&q=http://allafrica.com/tools/headlines/rdf/liberia/headlines.rdf");
+          social.rssParse2DB("http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&q=http://www.google.com/alerts/feeds/14472321683254243665/7898718268626814814");
+          social.rssParse2DB("http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&q=http://www.google.com/alerts/feeds/14472321683254243665/11564968275061407926");   
+          
+      }//end if
+      
+      if ("sync2Blog".equals(value))
+      {
+          //Push Feeds to Blogger
+          try {
+               social.syncDB2Blogger(secure.getBlogID(secure.LIBERIA_NEWS_BLOG));
+            } catch (Exception e) {
+                log.info("failed" + e);
               } finally {
           
               }
-
+       }//end if
   } 
 
 }
