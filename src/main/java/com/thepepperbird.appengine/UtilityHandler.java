bf126cb270c4b0bbaa0a09910ba7885a971bd94e
Kareem Glover <iamkhova@gmail.com>
2015-06-27 02:36:29 +0000

    Further improvement to the success of getting text from Jsoup scripts.

--- src/main/java/com/thepepperbird.appengine/UtilityHandler.java
+++ src/main/java/com/thepepperbird.appengine/UtilityHandler.java
@@ -15,8 +15,15 @@ import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.QueryResultList;
 import com.google.appengine.api.datastore.PreparedQuery;
 import java.util.logging.Logger; 
+import java.net.URLDecoder;
+
+import java.io.UnsupportedEncodingException; 
+import java.net.URLDecoder; 
+import java.util.regex.*; 
 
 public class UtilityHandler {
+  
+  public static final Logger log = Logger.getLogger(UtilityHandler.class.getName());
   
   public void cleanDB()
     {
@@ -39,18 +46,112 @@ public class UtilityHandler {
     
     }
   
-    public String cleanRSSLink (String _rssLink)
+    public String cleanRSSLink (String _rssLink) 
     {
       String tmpString = _rssLink;
       String tmpValue;
-      
-      
+
+      log.info("cleaning" + _rssLink);
       // Check url=
       tmpValue = "url=";
       if( tmpString.toLowerCase().contains(tmpValue.toLowerCase()) == true)
          {
-           tmpString = tmpString.substring(tmpString.indexOf("url=")+4);}
-         
+           tmpString = tmpString.substring(tmpString.indexOf("url=")+4);
+      log.info("cleaned" + tmpValue);}
+      /*
+      tmpValue = "%";
+      if( tmpString.toLowerCase().contains(tmpValue.toLowerCase()) == true)
+         {
+       try{
+        tmpString = convertURItoURL(tmpString);log.info("cleaned" + tmpValue);
+       }catch (UnsupportedEncodingException ex) {log.info("Failure with URL decode");} 
+      }
+      */
+      
+            tmpValue = "%3F";
+      if( tmpString.toLowerCase().contains(tmpValue.toLowerCase()) == true)
+         {
+        tmpString = tmpString.replace(tmpValue, "?");  log.info("cleaned" + tmpValue);}
+      
+            tmpValue = "%3D";
+      if( tmpString.toLowerCase().contains(tmpValue.toLowerCase()) == true)
+         {
+        tmpString = tmpString.replace(tmpValue, "=");  log.info("cleaned" + tmpValue);}
+      
+            tmpValue = "%26";
+      if( tmpString.toLowerCase().contains(tmpValue.toLowerCase()) == true)
+         {
+        tmpString = tmpString.replace(tmpValue, "&");  log.info("cleaned" + tmpValue);}
+      
+      
+      tmpValue = "&pg";
+      if( tmpString.toLowerCase().contains(tmpValue.toLowerCase()) == true)
+         {
+        tmpString = clearAfterPhrase(tmpValue, tmpString);
+        log.info("cleaned" + tmpValue); }
+      
+      tmpValue = "&ct";
+      if( tmpString.toLowerCase().contains(tmpValue.toLowerCase()) == true)
+         {
+        tmpString = clearAfterPhrase(tmpValue, tmpString);log.info("cleaned" + tmpValue); }
+      
+      tmpValue = "%E2%80%98";
+      if( tmpString.toLowerCase().contains(tmpValue.toLowerCase()) == true)
+         {
+        tmpString = tmpString.replace(tmpValue, "‘"); log.info("cleaned" + tmpValue);}
+      
+      tmpValue = "%E2%80%99";
+      if( tmpString.toLowerCase().contains(tmpValue.toLowerCase()) == true)
+         {
+        tmpString = tmpString.replace(tmpValue, "’");  log.info("cleaned" + tmpValue);}
+      
+      tmpValue = "%25E2%2580%2599";
+      if( tmpString.toLowerCase().contains(tmpValue.toLowerCase()) == true)
+         {
+        tmpString = tmpString.replace(tmpValue, "’"); log.info("cleaned" + tmpValue);}
+      
+        tmpValue = "%25E2%2580%2598";
+      if( tmpString.toLowerCase().contains(tmpValue.toLowerCase()) == true)
+         {
+        tmpString = tmpString.replace(tmpValue, "‘"); log.info("cleaned" + tmpValue);}
+      
+              tmpValue = "%2520";
+      if( tmpString.toLowerCase().contains(tmpValue.toLowerCase()) == true)
+         {
+        tmpString = tmpString.replace(tmpValue, " "); log.info("cleaned" + tmpValue);}
+                    tmpValue = "%20";
+      if( tmpString.toLowerCase().contains(tmpValue.toLowerCase()) == true)
+         {
+        tmpString = tmpString.replace(tmpValue, " "); log.info("cleaned" + tmpValue);}
+      
+      
+      
+         log.info("clean complete:" + tmpString);
       return tmpString;
     }
+  
+  public String convertURItoURL (String _url)  throws UnsupportedEncodingException
+    {
+    log.info("converting uri to url");
+    String tempURL = "";
+    try{
+      tempURL = java.net.URLDecoder.decode(_url, "UTF-8");
+    }catch (UnsupportedEncodingException ex) {log.info("Failure with URL decode");} 
+    return tempURL;
+    
+  }
+  
+  public String clearAfterPhrase (String _phrase, String _url)
+    {
+      String value; 
+    Pattern pattern = Pattern.compile(_phrase + ".*$", Pattern.DOTALL);
+    Matcher matcher = pattern.matcher(_url);
+   value = matcher.replaceFirst(""); 
+    
+    
+    return value;
+  }
+  
+  
+  
 }
